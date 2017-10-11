52adffea04422e33bd02b14b22f4dd77970717ae
Jackson Donovan <iinventedtwitter@gmail.com>
2017-10-11 15:31:49 -0600

    dealing 26 cards to each

--- app/app.module.js
+++ app/app.module.js
@@ -19,6 +19,28 @@ var app = angular.module('app',["ngRoute"])
                  })
                  .controller('homeCTRL',function($scope){
                    $scope.title = 'Welcome to War'
+                   $scope.cards = []
+                   $scope.getcards = function(){
+                     fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
+                     .then(function(reponse){
+                       console.log('status:',reponse.status);
+                       reponse.json().then(function(data){
+                         console.log(data.cards);
+                         for(var i = 0; i < data.cards.length; i++){
+                           $scope.cards.push(data.cards[i])
+                         }
+                       })
+                     })
+                     $scope.mycards = []
+                     $scope.yourcards = []
+                     for(let i = 1; i <= 26; i++){
+                       $scope.mycards.push($scope.cards[i])
+                     }
+                     for(let i = 27; i <= 52; i++){
+                       $scope.yourcards.push($scope.cards[i])
+                     }
+                   }
+
                  })
                  .controller('aboutCTRL',function($scope){
 
