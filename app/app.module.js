824b5c421b9d1eaf9029f713e6e930b4c67f6313
Jackson Donovan <iinventedtwitter@gmail.com>
2017-10-11 17:26:49 -0600

    hiding cards, making first set reveal, winner is shown

--- app/app.module.js
+++ app/app.module.js
@@ -19,28 +19,104 @@ var app = angular.module('app',["ngRoute"])
                  })
                  .controller('homeCTRL',function($scope){
                    $scope.title = 'Welcome to War'
+                   $scope.handvalue = function(card){
+                     switch (card) {
+                       case 'ACE':
+                         return 14;
+                         break;
+                       case 'KING':
+                         return 13;
+                         break;
+                       case 'QUEEN':
+                         return 12;
+                         break;
+                       case 'JACK':
+                         return 11;
+                         break;
+                       case '10':
+                         return 10;
+                         break;
+                       case '9':
+                         return 9;
+                         break;
+                       case '8':
+                         return 8;
+                         break;
+                       case '7':
+                         return 7;
+                         break;
+                       case '6':
+                         return 6;
+                         break;
+                       case '5':
+                         return 5;
+                         break;
+                       case '4':
+                         return 4;
+                         break;
+                       case '3':
+                         return 3;
+                         break;
+                       case '2':
+                         return 2;
+                         break;
+                       default:
+                     }
+                   }
                    $scope.cards = []
+                   $scope.cardimages = []
+                   $scope.handvalueofmycards = []
+                   $scope.handvalueofyourcards = []
                    $scope.getcards = function(){
-                     fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
+                     fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=12')
                      .then(function(reponse){
                        console.log('status:',reponse.status);
                        reponse.json().then(function(data){
                          console.log(data.cards);
                          for(var i = 0; i < data.cards.length; i++){
+                           $scope.cardimages.push(data.cards[i].image)
+                           data.cards[i].image = 'https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-fournier-505-1_grande.jpg?v=1494194154'
                            $scope.cards.push(data.cards[i])
                          }
                        })
                      })
                      $scope.mycards = []
                      $scope.yourcards = []
-                     for(let i = 1; i <= 26; i++){
+                     for(let i = 1; i <= 6; i++){
                        $scope.mycards.push($scope.cards[i])
+                       $scope.handvalueofmycards.push($scope.handvalue($scope.cards[i].value))
                      }
-                     for(let i = 27; i <= 52; i++){
+                     for(let i = 7; i <= 12; i++){
                        $scope.yourcards.push($scope.cards[i])
+                       $scope.handvalueofyourcards.push($scope.handvalue($scope.cards[i].value))
+                     }
+                     console.log($scope.handvalueofmycards);
+                   }
+                   console.log($scope.handvalueofmycards);
+                   console.log($scope.handvalueofmycards[0]);
+                   $scope.winner
+                   $scope.showwinner = function(){
+                     if($scope.handvalueofmycards[0] > $scope.handvalueofyourcards[0]){
+                       $scope.cards[0].image = $scope.cardimages[0]
+                       console.log('my card:' , $scope.handvalueofmycards[0]);
+                       console.log('your card:' , $scope.handvalueofyourcards[0]);
+                       console.log($scope.cardimages[0]);
+                       $scope.winner = 'I win the first round!'
+                     } else {
+                       $scope.cards[7].image = $scope.cardimages[7]
+                       console.log('my card:' , $scope.handvalueofmycards[0]);
+                       console.log('your card:' , $scope.handvalueofyourcards[0]);
+                       $scope.winner = 'You win the first round!'
                      }
                    }
-
+                   $scope.resetdeck = function(){
+                     $scope.cards = []
+                     $scope.handvalueofmycards = []
+                     $scope.handvalueofyourcards = []
+                     $scope.mycards = []
+                     $scope.yourcards = []
+                     $scope.winner = ''
+                   }
                  })
                  .controller('aboutCTRL',function($scope){
 
